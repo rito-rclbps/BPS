58685c74424a430dcd3aeab45127676b7d7a2741
rito@rcl-aoyama.jp <rito@rcl-aoyama.jp>
2013-06-09 19:51:37 +0900

    クロールに時間がかかる 未完成

--- Practice/Aozora/main.rb
+++ Practice/Aozora/main.rb
@@ -2,11 +2,23 @@
 require 'net/http'
 require './parse.rb'
 
-Net::HTTP.version_1_2
+aozora_list = [] # 最終的に得るリスト
 
+Net::HTTP.version_1_2
 person_all_response = Net::HTTP.start('www.aozora.gr.jp', 80) do |http|
   http.get('/index_pages/person_all.html')
 end
-
-author_page_list = Parse.make_author_page_list(person_all_response.body.force_encoding('utf-8'))
-
+Parse.make_author_list(person_all_response.body.force_encoding('utf-8'))
+Parse.get_book_list.each do |name, url|
+  book_response = Net::HTTP.start('www.aozora.gr.jp', 80) do |http|
+    http.get("/index_pages/#{url}")
+  end
+  booklist1, booklist2 = Parse.make_book_list(book_response.body.force_encoding('utf-8'))
+  aozora_list << booklist1.each do |book|
+    [name, '公開中の作品', book]
+  end
+  aozora_list << booklist2.each do |book|
+    [name, '作業中の作品', book]
+  end
+end
+p aozora_list
--- Practice/Aozora/parse.rb
+++ Practice/Aozora/parse.rb
@@ -1,8 +1,8 @@
 # -*- encoding: UTF-8 -*-
 
 class Parse
-  @@page_list = {}
-  def self.make_author_page_list(body)
+  @@page_list = {} # クラス変数にする意味がなかった
+  def self.make_author_list(body)
     body.each_line do |line|
       if line =~ /<li><a href=".+">.+<\/a>.+\(→/
         # なにもしない
@@ -11,6 +11,20 @@ class Parse
         @@page_list[$2] = $1
       end
     end
-p @@page_list
+  end
+  def self.get_book_list
+    @@page_list
+  end
+  def self.make_book_list(body)
+    book_list1 = [] # 公開中
+    book_list2 = [] # 作業中
+    body.each_line do |line|
+      if line =~/<li><a href=".+">(.+)<\/a>.+<\/li>/
+        book_list1 << $1
+      elsif line =~ /<li>(.+)\t<\/li>/
+        book_list2 << $1
+      end
+    end
+    return book_list1, book_list2
   end
 end
